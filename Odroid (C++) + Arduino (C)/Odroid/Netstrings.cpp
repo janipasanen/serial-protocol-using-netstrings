d374a5af68e3396daeafac4a81f128387d778593
Jani Pasanen <jani.m.pasanen@gmail.com>
2015-05-01 15:49:32 +0200

    Minor changes to Netstrings.cpp and adding the Proxy.cpp file to Odroid folder for reference

--- Odroid (C++) + Arduino (C)/Odroid/Netstrings.cpp
+++ Odroid (C++) + Arduino (C)/Odroid/Netstrings.cpp
@@ -2,7 +2,7 @@
 // Netstrings.cpp
 // Netstrings
 //
-//  Created by Jason Kuan on 29/04/15.
+//  Created by Jason Kuan & Jani Pasanen 2015.
 //  Copyright (c) 2015 Jason Kuan. All rights reserved.
 //
 
@@ -15,38 +15,38 @@ string encodeNetstring(string payload) {
     if(!len) return "ERROR_EMPTY_STRING";
     string s = to_string(len);
     string netstring = s + ":" + payload + ",";
-    
+
     return netstring;
 }
 
 string decodeNetstring(string netstring) {
     if (netstring.length() < 3) return "NETSTRING_ERROR_TOO_SHORT";
-    
+
     //if (netstring.length() > ?) return "NETSTRING_ERROR_TOO_LONG";
-    
+
     long semicolonIndex = netstring.find(':');
     if (semicolonIndex < 0) return "NETSTRING_ERROR_NO_COLON";
-    
+
     string getLength = netstring.substr(0, semicolonIndex);
     int payloadLength = atoi(getLength.c_str());            //Convert string to int
     if (payloadLength < 1) return "NETSTRING_ERROR_LEADING_ZERO";
-    
+
     string payload = netstring.substr(semicolonIndex+1);
     if (!payload.length()) return "NETSTRING_ERROR_NO_LENGTH";
-    
+
     if (payload.substr(payload.length() -1) == ",") payload.erase(payload.length()-1); //remove the comma
     if (payload.length() != payloadLength) return "NETSTRING_ERROR_INVALID_LENGTH";
-    
+
     return payload;
 }
 int main(int argc, const char * argv[]) {
 
     string payload = "michael is the BEST EVER!!!";
     string netstring = encodeNetstring(payload);
-    
+
     cout << encodeNetstring(payload) << endl;
     cout << decodeNetstring(netstring) << endl;
-    
+
     return 0;
-    
+
 }

9202eb034eb4cae6ffdc910478ba0edd1f4a635d
Jani Pasanen <jani.m.pasanen@gmail.com>
2015-05-01 13:07:02 +0000

    added sensor code for homeproject and Arduiono

---
+++ Raspberry Pi + Arduino + C/Arduino/Ultrasonic sensor 87891/example_87891/example_87891.ino
@@ -0,0 +1,21 @@
+ int inputPin=2; //ECHO pin 
+ int outputPin=4; //TRIG pin 
+
+ void setup() 
+ { 
+   Serial.begin(9600); 
+   pinMode(inputPin, INPUT); 
+   pinMode(outputPin, OUTPUT); 
+ }
+ 
+ void loop() 
+ { 
+   digitalWrite(outputPin, HIGH); //Trigger ultrasonic detection 
+   delayMicroseconds(10); 
+   digitalWrite(outputPin, LOW); 
+   int distance = pulseIn(inputPin, HIGH); //Read ultrasonic reflection
+   distance= distance/58; //Calculate distance 
+   Serial.println(distance); //Print distance 
+   delay(100); 
+ } 
+
